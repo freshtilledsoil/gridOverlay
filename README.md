575becc659f3e4705775fa279e644009833078b6
Steve Hickey <steve@stevehickeydesign.com>
2012-07-23 10:21:07 -0700

    Transition main project to jQuery plugin instead of SASS mixin, update Bourbon, add SASS gist to README

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 Grid Overlay
-================
+============
 
 A method for creating a grid overlay on your site. Allows you to easily align design elements to your grid.
+
+Please Note
+-----------
+
+The SASS mixin this project started with has been moved to a GitHub Gist as this project changes to a jQuery plugin. You can find it [here](https://gist.github.com/3013151) if you're interested.
---
+++ javascripts/gridOverlay.js
@@ -0,0 +1,32 @@
+/*
+
+  Grid Overlay Plugin
+
+  Author: Steve Hickey, UI Designer & Front-End Developer
+  https://twitter.com/stevehickeydsgn | http://stevehickeydesign.com/
+  Company: Fresh Tilled Soil
+  http://www.freshtilledsoil.com/ | https://github.com/javasteve99/gridOverlay
+
+  Licensed for use under the WTFPL | http://en.wikipedia.org/wiki/WTFPL
+  Free for all uses without attribution or credit. The author is not liable for
+  anything that doesn't work or breaks. You're on your own, you've been warned.
+
+*/
+
+(function gridOverlay() {
+  "use strict";
+
+  var page = 940,
+      column = 60,
+      gutter = 20,
+      baseline = 24,
+      gridMarkup = '<div id="gridOverlay" style="position: absolute; top: 0; right: 0; bottom: 0; left: 0; width: ' + page +'px; margin: 0 auto; background-size: ' + (column+gutter) +'px ' + baseline +'px; background-image: -webkit-linear-gradient(left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + column +'px, transparent ' + column +'px), -webkit-linear-gradient(top, transparent ' + (baseline-1) +'px, rgba(255, 0, 0, 0.5) ' + baseline +'px); background-image: -moz-linear-gradient(left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + column +'px, transparent ' + column +'px), -moz-linear-gradient(top, transparent ' + (baseline-1) +'px, rgba(255, 0, 0, 0.5) ' + baseline +'px); background-image: -ms-linear-gradient(left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + column +'px, transparent ' + column +'px), -ms-linear-gradient(top, transparent ' + (baseline-1) +'px, rgba(255, 0, 0, 0.5) ' + baseline +'px); background-image: -o-linear-gradient(left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + column +'px, transparent ' + column +'px), -o-linear-gradient(top, transparent ' + (baseline-1) +'px, rgba(255, 0, 0, 0.5) ' + baseline +'px); background-image: linear, left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + column +'px, transparent ' + column +'px, linear, top, transparent ' + (baseline-1) +'px, rgba(255, 0, 0, 0.5) ' + baseline +'px;"></div>',
+      body = $('body');
+
+  body.prepend(gridMarkup).css({
+    'position': 'relative',
+    'height': 'auto',
+    'min-height': '100%'
+  });
+
+}());
