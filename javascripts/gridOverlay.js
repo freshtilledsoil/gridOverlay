54907cce412097a30d8a620fdaa6d8b9f260a402
Steve Hickey <steve@stevehickeydesign.com>
2012-09-02 17:20:31 -0400

    Add toggle support and make documentation accurate

--- javascripts/gridOverlay.js
+++ javascripts/gridOverlay.js
@@ -16,17 +16,24 @@
 (function gridOverlay() {
   "use strict";
 
-  var page = 940,
-      column = 60,
-      gutter = 20,
-      baseline = 24,
-      gridMarkup = '<div id="gridOverlay" style="position: absolute; top: 0; right: 0; bottom: 0; left: 0; width: ' + page +'px; margin: 0 auto; background-size: ' + (column+gutter) +'px ' + baseline +'px; background-image: -webkit-linear-gradient(left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + column +'px, transparent ' + column +'px), -webkit-linear-gradient(top, transparent ' + (baseline-1) +'px, rgba(255, 0, 0, 0.5) ' + baseline +'px); background-image: -moz-linear-gradient(left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + column +'px, transparent ' + column +'px), -moz-linear-gradient(top, transparent ' + (baseline-1) +'px, rgba(255, 0, 0, 0.5) ' + baseline +'px); background-image: -ms-linear-gradient(left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + column +'px, transparent ' + column +'px), -ms-linear-gradient(top, transparent ' + (baseline-1) +'px, rgba(255, 0, 0, 0.5) ' + baseline +'px); background-image: -o-linear-gradient(left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + column +'px, transparent ' + column +'px), -o-linear-gradient(top, transparent ' + (baseline-1) +'px, rgba(255, 0, 0, 0.5) ' + baseline +'px); background-image: linear, left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + column +'px, transparent ' + column +'px, linear, top, transparent ' + (baseline-1) +'px, rgba(255, 0, 0, 0.5) ' + baseline +'px;"></div>',
+  var page = 940,               // set page width
+      column = 60,              // set column width
+      gutter = 20,              // set gutter width
+      baseline = 24,            // set baseline height
+      initialDisplay = 'none',  // use string 'block' to have grid on by default
+      gridMarkup = '<div id="gridOverlay" style="display: '+ initialDisplay +'; position: absolute; top: 0; right: 0; bottom: 0; left: 0; width: ' + page +'px; margin: 0 auto; background-size: ' + (column+gutter) +'px ' + baseline +'px; background-image: -webkit-linear-gradient(left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + column +'px, transparent ' + column +'px), -webkit-linear-gradient(top, transparent ' + (baseline-1) +'px, rgba(255, 0, 0, 0.5) ' + baseline +'px); background-image: -moz-linear-gradient(left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + column +'px, transparent ' + column +'px), -moz-linear-gradient(top, transparent ' + (baseline-1) +'px, rgba(255, 0, 0, 0.5) ' + baseline +'px); background-image: -ms-linear-gradient(left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + column +'px, transparent ' + column +'px), -ms-linear-gradient(top, transparent ' + (baseline-1) +'px, rgba(255, 0, 0, 0.5) ' + baseline +'px); background-image: -o-linear-gradient(left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + column +'px, transparent ' + column +'px), -o-linear-gradient(top, transparent ' + (baseline-1) +'px, rgba(255, 0, 0, 0.5) ' + baseline +'px); background-image: linear, left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + column +'px, transparent ' + column +'px, linear, top, transparent ' + (baseline-1) +'px, rgba(255, 0, 0, 0.5) ' + baseline +'px;"></div>',
       body = $('body');
 
   body.prepend(gridMarkup).css({
     'position': 'relative',
     'height': 'auto',
     'min-height': '100%'
+  });
+
+  $(document).keydown(function (e) {
+    if (e.keyCode == 71 && e.ctrlKey) {
+      $("#gridOverlay").toggle();
+    }
   });
 
 }());
