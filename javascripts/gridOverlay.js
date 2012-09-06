d3c8a31f5f93a72cfe0b33e601eebcd47ae78081
Steve Hickey <steve@stevehickeydesign.com>
2012-09-05 20:43:29 -0400

    Get closer to dropping jQuery dependence and minor bug fixes

--- javascripts/gridOverlay.js
+++ javascripts/gridOverlay.js
@@ -1,6 +1,6 @@
 /*
 
-  Grid Overlay Plugin
+  gridOverlay Plugin
 
   Author: Steve Hickey, UI Designer & Front-End Developer
   https://twitter.com/stevehickeydsgn | http://stevehickeydesign.com/
@@ -13,27 +13,29 @@
 
 */
 
-(function gridOverlay() {
+;(function() {
+
   "use strict";
 
-  var page = 940,               // set page width
-      column = 60,              // set column width
-      gutter = 20,              // set gutter width
-      baseline = 24,            // set baseline height
-      initialDisplay = 'none',  // use string 'block' to have grid on by default
-      gridMarkup = '<div id="gridOverlay" style="display: '+ initialDisplay +'; position: absolute; top: 0; right: 0; bottom: 0; left: 0; width: ' + page +'px; margin: 0 auto; background-size: ' + (column+gutter) +'px ' + baseline +'px; background-image: -webkit-linear-gradient(left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + column +'px, transparent ' + column +'px), -webkit-linear-gradient(top, transparent ' + (baseline-1) +'px, rgba(255, 0, 0, 0.5) ' + baseline +'px); background-image: -moz-linear-gradient(left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + column +'px, transparent ' + column +'px), -moz-linear-gradient(top, transparent ' + (baseline-1) +'px, rgba(255, 0, 0, 0.5) ' + baseline +'px); background-image: -ms-linear-gradient(left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + column +'px, transparent ' + column +'px), -ms-linear-gradient(top, transparent ' + (baseline-1) +'px, rgba(255, 0, 0, 0.5) ' + baseline +'px); background-image: -o-linear-gradient(left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + column +'px, transparent ' + column +'px), -o-linear-gradient(top, transparent ' + (baseline-1) +'px, rgba(255, 0, 0, 0.5) ' + baseline +'px); background-image: linear, left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + column +'px, transparent ' + column +'px, linear, top, transparent ' + (baseline-1) +'px, rgba(255, 0, 0, 0.5) ' + baseline +'px;"></div>',
-      body = $('body');
+  var options = {
+    'page' : 940,             // set page width
+    'column' : 60,            // set column width
+    'gutter' : 20,            // set gutter width
+    'baseline' : 24,          // set baseline height
+    'initialDisplay' : 'none' // use string 'block' to have grid visible by default
+  },
+  grid = document.createElement('div');
 
-  body.prepend(gridMarkup).css({
-    'position': 'relative',
-    'height': 'auto',
-    'min-height': '100%'
-  });
+  grid.id = 'gridOverlay';
+  grid.style.cssText = 'display: ' + options.initialDisplay + '; position: absolute; top: 0; right: 0; bottom: 0; left: 0; width: ' + options.page + 'px; margin: 0 auto; background-size: ' + ( options.column + options.gutter ) + 'px ' + options.baseline + 'px; background-image: -webkit-linear-gradient(left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + options.column + 'px, transparent ' + options.column + 'px), -webkit-linear-gradient(top, transparent ' + ( options.baseline - 1 ) + 'px, rgba(255, 0, 0, 0.5) ' + options.baseline + 'px); background-image: -moz-linear-gradient(left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + options.column + 'px, transparent ' + options.column + 'px), -moz-linear-gradient(top, transparent ' + ( options.baseline - 1 ) + 'px, rgba(255, 0, 0, 0.5) ' + options.baseline + 'px); background-image: -ms-linear-gradient(left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + options.column + 'px, transparent ' + options.column + 'px), -ms-linear-gradient(top, transparent ' + ( options.baseline - 1 ) + 'px, rgba(255, 0, 0, 0.5) ' + options.baseline + 'px); background-image: -o-linear-gradient(left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + options.column + 'px, transparent ' + options.column + 'px), -o-linear-gradient(top, transparent ' + ( options.baseline - 1 ) + 'px, rgba(255, 0, 0, 0.5) ' + options.baseline + 'px); background-image: linear, left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + options.column + 'px, transparent ' + options.column + 'px, linear, top, transparent ' + ( options.baseline - 1 ) + 'px, rgba(255, 0, 0, 0.5) ' + options.baseline + 'px;")';
+
+  document.body.appendChild(grid);
+  document.body.style.cssText = 'position: relative; height: auto; min-height: 100%';
 
   $(document).keydown(function (e) {
-    if (e.keyCode == 71 && e.ctrlKey) {
+    if (e.keyCode === 71 && e.altKey) {
       $("#gridOverlay").toggle();
     }
   });
 
-}());
+})();
