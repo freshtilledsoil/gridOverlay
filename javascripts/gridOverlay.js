0da6fd7e2793613d54d230a107ba00a8f17ec283
Steve Hickey <steve.hickey@freshtilledsoil.com>
2012-09-25 14:23:39 -0400

    Polish demo page and add more options

--- javascripts/gridOverlay.js
+++ javascripts/gridOverlay.js
@@ -18,16 +18,19 @@
   "use strict";
 
   var options = {
-    'page' : 940,             // set page width
-    'column' : 60,            // set column width
-    'gutter' : 20,            // set gutter width
-    'baseline' : 24,          // set baseline height
-    'initialDisplay' : 'none' // use string 'block' to have grid visible by default
+    'page' : 940,                                   // set page width
+    'column' : 60,                                  // set column width
+    'columnColor' : 'rgba(255, 0, 0, 0.25)',        // set column color
+    'gutter' : 20,                                  // set gutter width
+    'gutterColor' : 'transparent',                  // set gutter color
+    'baseline' : 24,                                // set baseline height
+    'baselineColor' : 'rgba(255, 255, 255, 0.5)',   // set baseline color
+    'initialDisplay' : 'none'                       // use 'block' to have grid visible by default
   },
   grid = document.createElement('div');
 
   grid.id = 'gridOverlay';
-  grid.style.cssText = 'display: ' + options.initialDisplay + '; position: absolute; top: 0; right: 0; bottom: 0; left: 0; width: ' + options.page + 'px; margin: 0 auto; background-size: ' + ( options.column + options.gutter ) + 'px ' + options.baseline + 'px; background-image: -webkit-linear-gradient(left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + options.column + 'px, transparent ' + options.column + 'px), -webkit-linear-gradient(top, transparent ' + ( options.baseline - 1 ) + 'px, rgba(255, 0, 0, 0.5) ' + options.baseline + 'px); background-image: -moz-linear-gradient(left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + options.column + 'px, transparent ' + options.column + 'px), -moz-linear-gradient(top, transparent ' + ( options.baseline - 1 ) + 'px, rgba(255, 0, 0, 0.5) ' + options.baseline + 'px); background-image: -ms-linear-gradient(left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + options.column + 'px, transparent ' + options.column + 'px), -ms-linear-gradient(top, transparent ' + ( options.baseline - 1 ) + 'px, rgba(255, 0, 0, 0.5) ' + options.baseline + 'px); background-image: -o-linear-gradient(left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + options.column + 'px, transparent ' + options.column + 'px), -o-linear-gradient(top, transparent ' + ( options.baseline - 1 ) + 'px, rgba(255, 0, 0, 0.5) ' + options.baseline + 'px); background-image: linear, left, rgba(255, 0, 0, 0.25) 0, rgba(255, 0, 0, 0.25) ' + options.column + 'px, transparent ' + options.column + 'px, linear, top, transparent ' + ( options.baseline - 1 ) + 'px, rgba(255, 0, 0, 0.5) ' + options.baseline + 'px;")';
+  grid.style.cssText = 'display: ' + options.initialDisplay + '; position: absolute; top: 0; right: 0; bottom: 0; left: 0; width: ' + options.page + 'px; margin: 0 auto; background-size: ' + ( options.column + options.gutter ) + 'px ' + options.baseline + 'px; background-image: -webkit-linear-gradient(left, ' + options.columnColor + ' 0, ' + options.columnColor + ' ' + options.column + 'px, ' + options.gutterColor + ' ' + options.column + 'px), -webkit-linear-gradient(top, transparent ' + ( options.baseline - 1 ) + 'px, ' + options.baselineColor + ' ' + options.baseline + 'px); background-image: -moz-linear-gradient(left, ' + options.columnColor + ' 0, ' + options.columnColor + ' ' + options.column + 'px, ' + options.gutterColor + ' ' + options.column + 'px), -moz-linear-gradient(top, transparent ' + ( options.baseline - 1 ) + 'px, ' + options.baselineColor + ' ' + options.baseline + 'px); background-image: -ms-linear-gradient(left, ' + options.columnColor + ' 0, ' + options.columnColor + ' ' + options.column + 'px, ' + options.gutterColor + ' ' + options.column + 'px), -ms-linear-gradient(top, transparent ' + ( options.baseline - 1 ) + 'px, ' + options.baselineColor + ' ' + options.baseline + 'px); background-image: -o-linear-gradient(left, ' + options.columnColor + ' 0, ' + options.columnColor + ' ' + options.column + 'px, ' + options.gutterColor + ' ' + options.column + 'px), -o-linear-gradient(top, transparent ' + ( options.baseline - 1 ) + 'px, ' + options.baselineColor + ' ' + options.baseline + 'px); background-image: linear, left, ' + options.columnColor + ' 0, ' + options.columnColor + ' ' + options.column + 'px, ' + options.gutterColor + ' ' + options.column + 'px, linear, top, transparent ' + ( options.baseline - 1 ) + 'px, ' + options.baselineColor + ' ' + options.baseline + 'px;")';
 
   document.body.appendChild(grid);
   document.body.style.cssText = 'position: relative; height: auto; min-height: 100%';
