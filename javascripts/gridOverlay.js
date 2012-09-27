bf30525b3ca21ff9b41795b9476818f6e372cdf3
Steve Hickey <steve@stevehickeydesign.com>
2012-09-26 22:08:24 -0400

    Closer to flexible grid

--- javascripts/gridOverlay.js
+++ javascripts/gridOverlay.js
@@ -14,26 +14,25 @@
   "use strict";
 
   var options = {
-    'pageWidth' :      '940px',                       // set page width (added as max-width for flexible grids)
-    'columnWidth' :    '60px',                        // set column width
-    'columnColor' :    'rgba(255, 0, 0, 0.25)',       // set column color
-    'gutterWidth' :    '20px',                        // set gutter width
-    'gutterColor' :    'transparent',                 // set gutter color
-    'widthUnits' :     'px',                          // set measurement units for column/gutter widths
-    'baselineHeight' : '24px',                        // set baseline height
-    'baselineColor' :  'rgba(255, 255, 255, 0.5)',    // set baseline color
-    'initialDisplay' : 'none'                         // use 'block' to have grid visible by default
+    'pageWidth' :        '940px',                       // set page width (added as max-width for flexible grids)
+    'columnWidth' :       60,                           // set column width (percentage = 6.382978723404)
+    'gutterWidth' :       20,                           // set gutter width (percentage = 2.127659574468)
+    'widthUnits' :       'px',                           // set measurement units for column/gutter widths
+    'baselineHeight' :    24,                           // set baseline height in pixels (for now)
+    'columnColor' :      'rgba(255, 0, 0, 0.25)',       // set column color
+    'baselineColor' :    'rgba(255, 255, 255, 0.5)',    // set baseline color
+    'initialDisplay' :   'none'                         // use 'block' to have grid visible by default
   },
-  columnGradient = options.columnColor + ' 0, ' + options.columnColor + ' ' + parseInt(options.columnWidth, 10) + 'px, ' + options.gutterColor + ' ' + parseInt(options.columnWidth, 10) + 'px',
-  baselineGradient = ( parseInt(options.baselineHeight, 10) - 1 ) + 'px, ' + options.baselineColor + ' ' + parseInt(options.baselineHeight, 10) + 'px',
+  columnGradient = options.columnColor + ' 0, ' + options.columnColor + ' ' + options.columnWidth + options.widthUnits + ', transparent ' + options.columnWidth + options.widthUnits,
+  baselineGradient = ( options.baselineHeight - 1 ) + 'px, ' + options.baselineColor + ' ' + options.baselineHeight + 'px',
   grid = document.createElement('div');
 
   grid.id = 'gridOverlay';
   grid.style.cssText = '\
     display: ' + options.initialDisplay + '; \
     position: absolute; top: 0; right: 0; bottom: 0; left: 0; margin: 0 auto; width: 100%; \
-    max-width: ' + parseInt(options.pageWidth, 10) + 'px; \
-    background-size: ' + ( parseInt(options.columnWidth, 10) + parseInt(options.gutterWidth, 10) ) + options.widthUnits + ' ' + parseInt(options.baselineHeight, 10) + 'px; \
+    max-width: ' + options.pageWidth + '; \
+    background-size: ' + ( options.columnWidth + options.gutterWidth ) + options.widthUnits + ' ' + options.baselineHeight + 'px; \
     background-image: -webkit-linear-gradient(left, ' + columnGradient + '), -webkit-linear-gradient(top, transparent ' + baselineGradient + '); \
     background-image:    -moz-linear-gradient(left, ' + columnGradient + '),    -moz-linear-gradient(top, transparent ' + baselineGradient + '); \
     background-image:     -ms-linear-gradient(left, ' + columnGradient + '),     -ms-linear-gradient(top, transparent ' + baselineGradient + '); \
@@ -42,7 +41,6 @@
 
   document.body.appendChild(grid);
   document.body.style.cssText = 'position: relative; height: auto; min-height: 100%';
-
   document.addEventListener('keyup', function (e) {
     if (e.keyCode === 71 && e.altKey) {
       var grid = document.getElementById('gridOverlay');
